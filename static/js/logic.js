cdfeb2cee544ec7643637ac907ef8a26af296556
VinnyShankar <vinayakshank@gmail.com>
2023-06-15 13:01:44 -0700

    obtain data from url

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,2 +1,9 @@
 // GeoJSON URL
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Obtain data from url
+d3.json(url)
+.then(data =>
+{
+  console.log(data)  
+})
