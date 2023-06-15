cec03efa999cdf81c16bd4b1d110728f9d6ac826
VinnyShankar <vinayakshank@gmail.com>
2023-06-15 13:04:47 -0700

    initialize map

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,8 +2,12 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Obtain data from url
-d3.json(url)
-.then(data =>
+d3.json(url).then(data =>
 {
-  console.log(data)  
+    // Log data to the console
+    console.log(data)
+
+    // Initialize map
+    let map = L.map("map")
+               .setView([37.09, -95.71],5)
 })
